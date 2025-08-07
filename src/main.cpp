7578cdd95c213ab2db5a444723d94fd783e38e80
Rex <rvo0428@163.com>
2025-08-07 14:24:12 +0800

    #Update ignore

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+
+void EntryA()
+{
+	std::cout << "Hello Concurrency World!" << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+	std::thread EntryAThread(EntryA);
+	EntryAThread.detach();
+
+	return 0;
+}
